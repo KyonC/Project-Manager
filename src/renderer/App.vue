85395aa16b7383dfa2d3fedc916fc33f02900540
Kyonko <9zhoou@gmail.com>
2019-04-05 09:42:56 +0800

    Initial
    Initial the Project

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'projectmanager'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
